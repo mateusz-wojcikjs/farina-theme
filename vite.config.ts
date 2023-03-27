5f108bc2c627ac379dba571e98b3973609787e17
Mateusz <mateusz-wojcik@hotmail.com>
2023-03-27 23:43:43 +0200

    handling js files on vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,33 @@
 // vite.config.js
-import { resolve, join } from 'path'
-import { glob, globSync, globStream, globStreamSync, Glob } from 'glob'
-import { defineConfig } from 'vite'
+import { join, resolve } from "path";
+import { globSync } from "glob";
+import { defineConfig } from "vite";
 
 const generateHtmlEntries = () => {
-    const entries = {};
-    globSync("**/*.html", {cwd: 'src'}).forEach(file => {
-        const name = file.replace(/\.html$/, '');
-        entries[name] = resolve(__dirname, 'src', file);
-    });
+  const entries = {};
+  globSync("**/*.html", { cwd: "src" }).forEach((file) => {
+    const name = file.replace(/\.html$/, "");
+    entries[name] = resolve(__dirname, "src", file);
+  });
 
-    return entries;
-}
+  return entries;
+};
 
 export default defineConfig({
-    root: join(__dirname, "src"),
-    build: {
-        outDir: join(__dirname, "dist"),
-        emptyOutDir: true,
-        minify: 'esbuild',
-        rollupOptions: {
-            input: generateHtmlEntries(),
-        },
-    },
-    optimizeDeps: {
-        include: ["@wordpress/scripts"]
+  root: join(__dirname, "src"),
+  build: {
+    outDir: join(__dirname, "dist"),
+    emptyOutDir: true,
+    minify: "esbuild",
+    rollupOptions: {
+      input: generateHtmlEntries(),
+      output: {
+        entryFileNames: "[name].[hash].js",
+        chunkFileNames: "[name].[hash].js"
+      }
     }
-})
+  },
+  optimizeDeps: {
+    include: ["@wordpress/scripts"]
+  }
+});
