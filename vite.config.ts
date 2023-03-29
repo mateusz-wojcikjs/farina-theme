11effa99291a2a3d94854d6f836cf7e328588129
Mateusz <mateusz-wojcik@hotmail.com>
2023-03-29 22:49:13 +0200

    upload media

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { join, resolve } from "path";
 import { globSync } from "glob";
 import { defineConfig } from "vite";
+import url from "@rollup/plugin-url";
 
 const generateHtmlEntries = () => {
   const entries = {};
@@ -31,13 +32,21 @@ const entries = {
 
 export default defineConfig({
   root: join(__dirname, "src"),
+  assetsInclude: ["**/*.mp4"],
   build: {
+    assetsInlineLimit: 20000,
     outDir: join(__dirname, "dist"),
     emptyOutDir: true,
     minify: "esbuild",
     rollupOptions: {
       input: entries,
-      plugins: []
+      plugins: [
+        url({
+          include: ["**/*.mp4", "**/*.webm"],
+          limit: 0, // Set to 0 to disable inlining and force files to be emitted to the output directory
+          emitFiles: true // Tell the plugin to emit the files to the output directory
+        })
+      ]
     }
   }
 });
